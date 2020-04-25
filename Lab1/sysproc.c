3f9512b84a08bbd7a67cd379b049f156741760d4
Raoul <Desktop+Raove@users.noreply.github.com>
2020-04-24 17:14:02 -0700

    Update sysproc.c

--- Lab1/sysproc.c
+++ Lab1/sysproc.c
@@ -7,94 +7,98 @@
 #include "mmu.h"
 #include "proc.h"
 
-int waitpid(int, int*, int);
-
 int
 sys_fork(void)
 {
-    return fork();
+  return fork();
 }
 
+/* MODIFIED: Lab 1 Part A
+*/
 int
 sys_exit(void)
 {
-    int status;
-    if(argint(0, &status) < 0){
-        return -1;
-    }
-    exit(status);
-    return 0;  // not reached
+  int status;
+  if(argint(0, &status) < 0){
+    return -1;
+  }
+  exit(status);
+  return 0;  // not reached
 }
 
+/* MODIFED: Lab 1 Part B
+*/
 int
 sys_wait(void)
 {
-    int *status;
-    if( argptr(0, (char**)&status, sizeof(status)) < 0){
-        return -1;
-    }
-    return wait(status);
+  int *status;
+  if( argptr(0, (char**)&status, sizeof(status)) < 0){
+    return -1;
+  }
+  return wait(status);
 }
-//TA explained during discussion and said just copy the wait above and add pid
+
+/* ADDED: Lab 1 Part C */
 int
 sys_waitpid(void)
 {
-    int *status;
-    int pid = 0;
-    if( argptr(1, (char**)&status, sizeof(*status)) < 0){
-        return -1;
-    }
-    return waitpid(pid, status, 0);
+  int *status;
+  int pid = 0;
+  if( argptr(1, (char**)&status, sizeof(*status)) < 0){
+    return -1;
+  }
+  return waitpid(pid, status, 0);
 }
+
 int
 sys_kill(void)
 {
-    int pid;
+  int pid;
 
-    if(argint(0, &pid) < 0)
-        return -1;
-    return kill(pid);
+  if(argint(0, &pid) < 0)
+    return -1;
+  return kill(pid);
 }
 
 int
 sys_getpid(void)
 {
-    return myproc()->pid;
+  return myproc()->pid;
 }
 
 int
 sys_sbrk(void)
 {
-    int addr;
-    int n;
+  int addr;
+  int n;
 
-    if(argint(0, &n) < 0)
-        return -1;
-    addr = myproc()->sz;
-    if(growproc(n) < 0)
-        return -1;
-    return addr;
+  if(argint(0, &n) < 0)
+    return -1;
+  addr = myproc()->sz;
+  if(growproc(n) < 0)
+    return -1;
+  return addr;
 }
 
 int
 sys_sleep(void)
 {
-    int n;
-    uint ticks0;
+  int n;
+  uint ticks0;
 
-    if(argint(0, &n) < 0)
-        return -1;
-    acquire(&tickslock);
-    ticks0 = ticks;
-    while(ticks - ticks0 < n){
-        if(myproc()->killed){
-            release(&tickslock);
-            return -1;
-        }
-        sleep(&ticks, &tickslock);
+  if(argint(0, &n) < 0)
+    return -1;
+  acquire(&tickslock);
+  ticks0 = ticks;
+  while(ticks - ticks0 < n){
+    if(myproc()->killed){
+      release(&tickslock);
+      return -1;
     }
-    release(&tickslock);
-    return 0;
+    sleep(&ticks, &tickslock);
+  }
+  release(&tickslock);
+  return 0;
 }
 
 // return how many clock tick interrupts have occurred
@@ -102,10 +106,10 @@ sys_sleep(void)
 int
 sys_uptime(void)
 {
-    uint xticks;
+  uint xticks;
 
-    acquire(&tickslock);
-    xticks = ticks;
-    release(&tickslock);
-    return xticks;
+  acquire(&tickslock);
+  xticks = ticks;
+  release(&tickslock);
+  return xticks;
 }
