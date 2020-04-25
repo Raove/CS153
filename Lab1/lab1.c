ac4e74ccda9c526c70526e5d5428fc61f112c432
Raoul <Desktop+Raove@users.noreply.github.com>
2020-04-24 18:37:19 -0700

    Update lab1.c

--- Lab1/lab1.c
+++ Lab1/lab1.c
@@ -50,6 +50,7 @@ int exitWaitTest(void) {
 int waitPidTest(void){
     int ret_pid, exit_status;
     int i;
+    int options = 0;
     int pid_a[5]={0, 0, 0, 0, 0};
     // use this part to test wait(int pid, int* status, int options)
     printf(1, "\n  C: testing waitpid(int pid, int* status, int options):\n");
@@ -60,23 +61,23 @@ int waitPidTest(void){
             exit(0);}}
     sleep(5);
     printf(1, "\n Parent: Waiting for child with PID# %d\n",pid_a[3]);
-    ret_pid = waitpid(pid_a[3], &exit_status, 0);
+    ret_pid = waitpid(pid_a[3], &exit_status, options);
     printf(1, "\n Parent: Child# %d has exited with %d on status\n",ret_pid, exit_status);
     sleep(5);
     printf(1, "\n Parent: Waiting for child with PID# %d\n",pid_a[1]);
-    ret_pid = waitpid(pid_a[1], &exit_status, 0);
+    ret_pid = waitpid(pid_a[1], &exit_status, options);
     printf(1, "\n Parent: Child# %d has exited with %d on status\n",ret_pid, exit_status);
     sleep(5);
     printf(1, "\n Parent: Waiting for child with PID# %d\n",pid_a[2]);
-    ret_pid = waitpid(pid_a[2], &exit_status, 0);
+    ret_pid = waitpid(pid_a[2], &exit_status, options);
     printf(1, "\n Parent: Child# %d has exited with %d on status\n",ret_pid, exit_status);
     sleep(5);
     printf(1, "\n Parent: Waiting for child with PID# %d\n",pid_a[0]);
-    ret_pid = waitpid(pid_a[0], &exit_status, 0);
+    ret_pid = waitpid(pid_a[0], &exit_status, options);
     printf(1, "\n Parent: Child# %d has exited with %d on status\n",ret_pid, exit_status);
     sleep(5);
     printf(1, "\n Parent: Waiting for child with PID# %d\n",pid_a[4]);
-    ret_pid = waitpid(pid_a[4], &exit_status, 0);
+    ret_pid = waitpid(pid_a[4], &exit_status, options);
     printf(1, "\n Parent: Child# %d has exited with %d on status\n",ret_pid, exit_status);
     return 0;
 }
