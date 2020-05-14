440a027e4e94433fb5faf49331b5f535e01b6c8e
Raoul <6421655+Raove@users.noreply.github.com>
2020-05-13 19:06:11 -0700

    Update syscall.c

--- Lab2/syscall.c
+++ Lab2/syscall.c
@@ -97,18 +97,25 @@ extern int sys_mknod(void);
 extern int sys_open(void);
 extern int sys_pipe(void);
 extern int sys_read(void);
+extern int sys_setpriority(void);
+extern int sys_getpriority(void);
+extern int sys_donate(void);
 extern int sys_sbrk(void);
 extern int sys_sleep(void);
 extern int sys_unlink(void);
 extern int sys_wait(void);
+extern int sys_waitpid(void);
 extern int sys_write(void);
 extern int sys_uptime(void);
-extern int sys_waitpid(void);
 
 static int (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
 [SYS_exit]    sys_exit,
 [SYS_wait]    sys_wait,
+[SYS_waitpid] sys_waitpid,
+[SYS_setpriority] sys_setpriority,
+[SYS_getpriority] sys_getpriority,
+[SYS_donate]  sys_donate,
 [SYS_pipe]    sys_pipe,
 [SYS_read]    sys_read,
 [SYS_kill]    sys_kill,
@@ -127,7 +134,6 @@ static int (*syscalls[])(void) = {
 [SYS_link]    sys_link,
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
-[SYS_waitpid]   sys_waitpid,
 };
 
 void
