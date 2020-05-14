38042c543296875c01e8692284432943393343d7
Raoul <6421655+Raove@users.noreply.github.com>
2020-05-13 19:04:30 -0700

    Update sh.c

--- Lab2/sh.c
+++ Lab2/sh.c
@@ -84,7 +84,7 @@ runcmd(struct cmd *cmd)
     close(rcmd->fd);
     if(open(rcmd->file, rcmd->mode) < 0){
       printf(2, "open %s failed\n", rcmd->file);
-      exit(0);
+      exit(1);
     }
     runcmd(rcmd->cmd);
     break;
@@ -175,7 +175,7 @@ void
 panic(char *s)
 {
   printf(2, "%s\n", s);
-  exit(0);
+  exit(1);
 }
 
 int
