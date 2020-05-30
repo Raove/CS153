136d52ed1bf69963e7ee5ad6c50f448bac362f19
Raoul <6421655+Raove@users.noreply.github.com>
2020-05-30 12:55:54 -0700

    stuff

---
+++ Lab3/lab2-1.c
@@ -0,0 +1,18 @@
+#include "types.h"
+#include "user.h"
+
+int main()
+{
+    printf(1, "Lab 2-1 with priority 30\n");
+    setpriority(30);
+    int i;
+    int j;
+    for(i = 0; i < 43000; i++){
+        asm("nop");
+        for(j = 0; j < 3000; j++){
+            asm("nop");
+        }
+    }
+    printf(1, "Lab2-1 has finished\n");
+    exit(0);
+}
