3fe4d5185454151381432157bf2a1153b8c8bdde
Raoul <6421655+Raove@users.noreply.github.com>
2020-06-05 18:39:56 -0700

    Update shm.c

--- Lab4/shm.c
+++ Lab4/shm.c
@@ -8,42 +8,91 @@
 #include "spinlock.h"
 
 struct {
-  struct spinlock lock;
-  struct shm_page {
-    uint id;
-    char *frame;
-    int refcnt;
-  } shm_pages[64];
+    struct spinlock lock;
+    struct shm_page {
+        uint id;
+        char *frame;
+        int refcnt;
+    } shm_pages[64];
 } shm_table;
 
 void shminit() {
-  int i;
-  initlock(&(shm_table.lock), "SHM lock");
-  acquire(&(shm_table.lock));
-  for (i = 0; i< 64; i++) {
-    shm_table.shm_pages[i].id =0;
-    shm_table.shm_pages[i].frame =0;
-    shm_table.shm_pages[i].refcnt =0;
-  }
-  release(&(shm_table.lock));
+    int i;
+    initlock(&(shm_table.lock), "SHM lock");
+    acquire(&(shm_table.lock));
+    for (i = 0; i< 64; i++) {
+        shm_table.shm_pages[i].id =0;
+        shm_table.shm_pages[i].frame =0;
+        shm_table.shm_pages[i].refcnt =0;
+    }
+    release(&(shm_table.lock));
 }
 
 int shm_open(int id, char **pointer) {
+    //you write this
+    int i, perms;
+    pte_t* pgdir;
+    uint va, pa;
 
-//you write this
+    acquire(&(shm_table.lock));
+
+    // If segment already exists
+    for(i = 0; i < 64; i++){
+        if(shm_table.shm_pages[i].id == id){
+            pgdir = myproc()->pgdir;
+            va = PGROUNDUP(myproc()->sz);
+            pa = V2P(shm_table.shm_pages[i].frame);
+            perms = PTE_W|PTE_U;
+            shm_table.shm_pages[i].refcnt += 1;
+            mappages(pgdir, (void*)va, PGSIZE, pa, perms);
+            myproc()->sz += PGSIZE;
+            *pointer = (char*)va;
+            release( &(shm_table.lock) );
+            return 0;
+        }
+    }
 
 
+    // If segment does not exist
+    for(i = 0; i < 64; i++){
+        if(shm_table.shm_pages[i].id == 0){
+            shm_table.shm_pages[i].id = id;
+            shm_table.shm_pages[i].frame = kalloc();
+            shm_table.shm_pages[i].refcnt = 1;
 
+            memset(shm_table.shm_pages[i].frame, 0, PGSIZE);
+            pgdir = myproc()->pgdir;
+            va = PGROUNDUP(myproc()->sz);
+            pa = V2P(shm_table.shm_pages[i].frame);
+            perms = PTE_W|PTE_U;
+            mappages(pgdir, (void*)va, PGSIZE, pa, perms);
+            myproc()->sz += PGSIZE;
+            *pointer = (char*)va;
+            release(&(shm_table.lock));
+            return 0;
+        }
+    }
 
-return 0; //added to remove compiler warning -- you should decide what to return
+    release( &(shm_table.lock) );
+    return 0; //added to remove compiler warning -- you should decide what to return
 }
 
 
 int shm_close(int id) {
-//you write this too!
+    //you write this too!
+    int i;
+    acquire(&(shm_table.lock));
+    for(i = 0; i < 64; i++){
+        if(shm_table.shm_pages[i].id == id){
+            shm_table.shm_pages[i].refcnt--;
+            if(shm_table.shm_pages[i].refcnt == 0){
+                shm_table.shm_pages[i].id = 0;
+                shm_table.shm_pages[i].frame = 0;
+                shm_table.shm_pages[i].refcnt = 0;
+            }
+        }
+    }
 
-
-
-
-return 0; //added to remove compiler warning -- you should decide what to return
+    release(&(shm_table.lock));
+    return 0; //added to remove compiler warning -- you should decide what to return
 }
